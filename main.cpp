63d8a3c7fc840fbd124a5dbe103044d014e26814
kumatheworld <kumatheworld1105@yahoo.co.jp>
2017-05-04 14:06:29 +0900

    add are()

--- main.cpp
+++ main.cpp
@@ -1,19 +1,40 @@
 #include <iostream>
+#include <string.h>
 #include "hilo.h"
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
+string strategy(Hilo k) {
+	if (k.max == k.h)
+		return "(H)";
+	if (k.max == k.l)
+		return "(L)";
+	return "";
+}
 
-	cout << "Number\tw/o JK\tw/ JK" << endl;
-	cout << "-------------------------" << endl;
-	Hilo res_wojk, res;
-	for (int i = 0; i < 13; ++i) {
-		res_wojk = exppo_wojk(1, Number(i));
-		res = exppo(1, Number(i));
-		cout << string_of_number[i] << "\t"
-		<< res_wojk.max << "\t"<< res.max << endl;
-	}
+void are(void) {
+	double win[] = {0.8, 1, 1.6, 2, 2.4, 3};
 	
+	for (int i = 0; i < 6; ++i) {
+		double r = win[i];
+		cout << "\nwin = " << r << "\nRank\tw/o JK\t\tw/ JK"
+		<< "\n-----------------------------------------------\n";
+		Hilo res_wojk, res;
+		for (int j = 0; j < 13; ++j) {
+			res_wojk = exppo_wojk(r, Number(j));
+			res = exppo(r, Number(j));
+			cout << string_of_number[j] << "\t"
+			<< res_wojk.max << "\t"
+			<< strategy(res_wojk) << "\t"
+			<< res.max << "\t" << strategy(res) << endl;
+		}
+	}
+}
+
+void table_gen(void) {
+}
+
+int main(int argc, char *argv[]) {
+	are();	
 	return 0;
 }
